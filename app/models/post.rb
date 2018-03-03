c0d1b91dbe36cc476c403f9610b8a41cf768a3e7
vagrant <vagrant@localhost.localdomain>
2018-03-03 15:39:01 +0900

    初こみ

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  # @post.comments
+  validates :title, presence: true, length: {minimum: 3, message: 'タイトルが短すぎます'}
+  validates :body, presence: true
+end
